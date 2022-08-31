885e20732b91176724ea4044ad4066505b8c6c45
Thomson, Molly Rita <mthomson39@gatech.edu>
2022-08-31 10:26:58 -0400

    More Attempts

---
+++ LongestCommonPrefix_14/longcommonpre.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+        string current = "";
+        for (auto& st : strs)
+        {
+            
+        }
+    }
+};
